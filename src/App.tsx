2f51e7a115b18f0e1c336ce78fa70ce2d8e15941
Louistam888 <louistam888@gmail.com>
2024-08-18 21:41:47 -0400

    starting app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,7 @@
 import "./App.css";
 
 function App() {
-  let name: string;
-  let age: number;
-  let isStudent: boolean;
-  let hobbies: string[];
-  let role: [number, string];
-
-  type Person = {
-    name: string;
-    age?: number;
-  };
-
-  let person: Person = {
-    name: "Piyush",
-    age: 20,
-  };
-
-  let lotsOfPeople: Person[];
-
-  //957
+//1818
   return (
     <>
       <p>Hello world</p>
